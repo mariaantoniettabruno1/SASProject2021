14b36d291988a81e43225f97a16accd3ccc50e83
Maria Antonietta Bruno <mariaantonietta.bruno24@gmail.com>
2021-11-29 17:31:14 +0100

    Inzio  costruzione cookingTask e summarySheet

--- src/businesslogic/kitchenTask/cookingTask.java
+++ src/businesslogic/kitchenTask/cookingTask.java
@@ -1,4 +1,66 @@
 package businesslogic.kitchenTask;
 
+import businesslogic.recipe.Recipe;
+import businesslogic.turn.Turn;
+
+import java.util.ArrayList;
+
 public class cookingTask {
+    Recipe recipe;
+    ArrayList<Turn> turn;
+    double preparation_time;
+    int quantity;
+    int difficulty;
+    int portions;
+    int importance;
+
+    public cookingTask(Recipe recipe, ArrayList<Turn> turn, double preparation_time, int quantity, int difficulty, int portions, int importance) {
+        this.recipe = recipe;
+        this.turn = turn;
+        this.preparation_time = preparation_time;
+        this.quantity = quantity;
+        this.difficulty = difficulty;
+        this.portions = portions;
+        this.importance = importance;
+    }
+
+    public double getPreparation_time() {
+        return preparation_time;
+    }
+
+    public void setPreparation_time(double preparation_time) {
+        this.preparation_time = preparation_time;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public int getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(int difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public int getPortions() {
+        return portions;
+    }
+
+    public void setPortions(int portions) {
+        this.portions = portions;
+    }
+
+    public int getImportance() {
+        return importance;
+    }
+
+    public void setImportance(int importance) {
+        this.importance = importance;
+    }
 }
--- src/businesslogic/kitchenTask/summarySheet.java
+++ src/businesslogic/kitchenTask/summarySheet.java
@@ -8,17 +8,19 @@ import java.util.ArrayList;
 public class summarySheet {
     private ArrayList<cookingTask> sheetCookingTask;
 
-    public void addCookingTask(ArrayList<Turn> turn, double preparation_time, int quantity, int portions, int difficulty, int importance, Recipe recipe){
-
-    }
-    public void deleteCookingTask(cookingTask ctsk){
-
-    }
-    public void CookingTask(ArrayList<Turn> turn, double preparation_time, int quantity, int portions, int difficulty, int importance, Recipe recipe, cookingTask ctsk){
+    public void addCookingTask(ArrayList<Turn> turn, double preparation_time, int quantity, int portions, int difficulty, int importance, Recipe recipe) {
 
     }
 
-    public void orderSummarySheet(boolean sortedByImportance, boolean sortedByDifficulty){
+    public void deleteCookingTask(cookingTask ctsk) {
+
+    }
+
+    public void CookingTask(ArrayList<Turn> turn, double preparation_time, int quantity, int portions, int difficulty, int importance, Recipe recipe, cookingTask ctsk) {
+
+    }
+
+    public void orderSummarySheet(boolean sortedByImportance, boolean sortedByDifficulty) {
 
     }
 
